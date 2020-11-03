b96db81d461696533ada15c9506540d1c11cc819
Allison Howard <allisonhoward309@gmail.com>
2020-11-02 18:42:37 -0600

    final

---
+++ IceCreamShop/FlavorComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+
+    class FlavorComparer : IComparer<Flavor>
+    {
+        public FlavorComparer()
+        {
+        }
+/*        public int Compare(Flavor x, Flavor y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }*/
+        public int Compare(Flavor x, Flavor y)
+        {
+            int diff = x.Allergens.Count - y.Allergens.Count;
+            return diff;
+
+        }
+
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,22 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
+            availableFlavors.Sort(new FlavorComparer());
+            foreach (Flavor option in availableFlavors)
+            {
+                Console.WriteLine(option);
+            }
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+            availableCones.Sort(new ConeComparer());
+            foreach (Cone option in availableCones)
+            {
+                Console.WriteLine(option);
+            }
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
-
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
     }
 }
